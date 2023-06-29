296f598b3d88d881ea767f32d6a1e834e7ed087f
BimoChan07 <bmochanb@gmail.com>
2023-06-29 23:10:57 +0545

    modification in file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +15,22 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from "./App"; // Assuming this is your main component
+import SignupPage from "./SignupPage"; // Import your signup page component
+import LoginPage from "./LoginPage"; // Import your login page component
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/signup" component={SignupPage} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
