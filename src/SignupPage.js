4ef9dc83d148b1f3251cba7c11527102933cec0d
BimoChan07 <bmochanb@gmail.com>
2023-06-29 23:11:22 +0545

    signup page added

---
+++ src/SignupPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+      {/* Add your sign-up form and other content here */}
+    </div>
+  );
+};
+
+export default SignupPage;
