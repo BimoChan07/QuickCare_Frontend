406f85f7ec6e6e4c44429e9615fb75de89a4f39a
BimoChan07 <bmochanb@gmail.com>
2023-07-01 19:08:20 +0545

    homepage added

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h2>Welcome to QuickCare - Doctor Consultation App</h2>
+      <p>Find and consult with expert doctors online.</p>
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default Homepage;
