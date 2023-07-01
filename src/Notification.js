ab36275810c61856455f4cb9c6e4cb6cb6abc4cf
BimoChan07 <bmochanb@gmail.com>
2023-07-01 18:55:13 +0545

    Notification modified

--- src/Notification.js
+++ src/Notification.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const NotificationsPage = () => {
   const [notifications, setNotifications] = useState([
-    { id: 1, title: 'New Appointment', content: 'You have a new appointment with Dr. John Doe.' },
-    { id: 2, title: 'Payment Received', content: 'Payment of $100 has been received.' },
-    { id: 3, title: 'Reminder', content: 'Don't forget your upcoming appointment tomorrow.' },
+    {
+      id: 1,
+      title: "New Appointment",
+      content: "You have a new appointment with Dr. John Doe.",
+    },
+    {
+      id: 2,
+      title: "Payment Received",
+      content: "Payment of $100 has been received.",
+    },
+    {
+      id: 3,
+      title: "Reminder",
+      content: "Don't forget your upcoming appointment tomorrow.",
+    },
   ]);
 
   const markAsRead = (id) => {
@@ -20,11 +32,16 @@ const NotificationsPage = () => {
       {notifications.length > 0 ? (
         <ul>
           {notifications.map((notification) => (
-            <li key={notification.id} className={!notification.read ? 'unread' : ''}>
+            <li
+              key={notification.id}
+              className={!notification.read ? "unread" : ""}
+            >
               <h3>{notification.title}</h3>
               <p>{notification.content}</p>
               {!notification.read && (
-                <button onClick={() => markAsRead(notification.id)}>Mark as Read</button>
+                <button onClick={() => markAsRead(notification.id)}>
+                  Mark as Read
+                </button>
               )}
             </li>
           ))}
