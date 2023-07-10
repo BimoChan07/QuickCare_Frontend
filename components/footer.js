f74933eea10ed3ff801cc00b266f00dbafaa55e8
BimoChan07 <bmochanb@gmail.com>
2023-07-10 22:47:30 +0545

    footer code added

---
+++ components/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="">
+        <div className="">
+          <div className="">
+            <p>&copy; 2023 QuickCare. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
