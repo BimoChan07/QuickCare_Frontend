8784dff753fae9553a735aa94964ea7242982faf
BimoChan07 <bmochanb@gmail.com>
2023-07-12 00:01:34 +0545

    form component added

--- components/form.js
+++ components/form.js
@@ -1,7 +1,45 @@
 import React from "react";
 
-const Form = () => { 
-    return (
-        
-    );
+const Form = () => {
+  return (
+    <form className="form">
+      <div className="form__group">
+        <label className="form__label" htmlFor="name">
+          Name
+        </label>
+        <input
+          className="form__input"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+        />
+      </div>
+      <div className="form__group">
+        <label className="form__label" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="form__input"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+        />
+      </div>
+      <div className="form__group">
+        <label className="form__label" htmlFor="message">
+          Message
+        </label>
+        <textarea
+          className="form__input"
+          id="message"
+          name="message"
+          placeholder="Message"
+        ></textarea>
+      </div>
+    </form>
+  );
 };
+
+export default Form;
