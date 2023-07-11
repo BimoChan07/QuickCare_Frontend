b5fa4e2851debf5e308df405560ba2697d89391d
BimoChan07 <bmochanb@gmail.com>
2023-07-11 23:59:03 +0545

    button component added

--- components/button.js
+++ components/button.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+const button = () => {
+  return (
+    <button className="button">
+      <div className="button__text">
+        <p>Button</p>
+      </div>
+    </button>
+  );
+};
+export default button;
